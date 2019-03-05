0c68c3f40867131a262ea5bd39fea94424963e99
Dylan Buehler <dylanbuehler1999@gmail.com>
2019-03-04 18:31:15 -0600

    Created the skeleton classes for the projects

---
+++ LinkedLists/LinkedLists/LinkedLists/LLNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    class LLNode
+    {
+        private int data;
+        private int next;
+
+        public int Data { get => data; set => data = value; }
+        public int Next { get => next; set => next = value; }
+
+        public LLNode()
+        {
+            data = Data;
+            next = Next;
+        }//End Constructor
+    }//End Class LLNode
+}//End Namespace
