62e0608ad65349ba3eb24053f5919557c4544c2b
Dylan Buehler <dylanbuehler1999@gmail.com>
2019-03-05 11:05:58 -0600

    Finished the skeletal frame for both classes

--- LinkedLists/LinkedLists/LinkedLists/LinkedList.cs
+++ LinkedLists/LinkedLists/LinkedLists/LinkedList.cs
@@ -8,5 +8,19 @@ namespace LinkedLists
 {
     class LinkedList
     {
-    }
-}
+        private LLNode front;
+        private LLNode back;
+        private LLNode current;
+
+        public LLNode Front { get => front; set => front = value; }
+        public LLNode Back { get => back; set => back = value; }
+        public LLNode Current { get => current; set => current = value; }
+
+        public LinkedList()
+        {
+            front = Front;
+            back = Back;
+            current = Current;
+        }//End Linked List Constructor
+    }//End Linked List
+}//End Namespace
