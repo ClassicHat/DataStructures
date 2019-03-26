bb73a6cdbd7aa2bf29b3065d54f2079045269428
Dylan Buehler <dylanbuehler1999@gmail.com>
2019-03-26 17:44:07 -0500

    Initial submission of the array based stack

---
+++ ArrayBasedStack/ArrayBasedStack/ArrayBasedStack/ArrayStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayBasedStack
+{
+    class ArrayStack
+    {
+        private int capacity;
+        private int size;
+        private ArrayList stack;
+
+        public int Capacity { get => capacity; set => capacity = value; }
+        public int Size { get => size; set => size = value; }
+        public ArrayList Stack { get => stack; set => stack = value; }
+
+        //Default Constructor
+        public ArrayStack(ArrayList theStack, int maxSize)
+        {
+            Stack = theStack;
+            Size = maxSize;
+        }//End Constructor
+
+        public static void Push(ArrayList theStack)
+        {
+            
+        }//End Push Method
+
+        public static void Pop(ArrayList theStack)
+        {
+
+        }//End Pop Method
+
+        public static void Peek(ArrayList theStack)
+        {
+
+        }//End Peek Method
+    }//End Class
+}//End Namespace
