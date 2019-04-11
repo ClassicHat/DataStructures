01c116a24f7024a8e87020416a86fbaa5474336b
Dylan Buehler <dylanbuehler1999@gmail.com>
2019-04-11 11:33:30 -0500

    Initial commit of the Binary tree project

---
+++ BinaryTree/BinaryTree/BinaryTree/BinaryTreeNode.cs
@@ -0,0 +1,28 @@
+﻿/****[ READ ME ]***************
+ * Author: Dylan Buehler
+ * Date: 4/11/2019
+ * Filename: BinaryTreeNode.cs
+ * Copyright: DilCoInc 2019
+ ******************************/
+namespace BinaryTree
+{
+    class BinaryTreeNode
+    {
+        //Private Fields
+        private char letter;
+        private BinaryTreeNode left;
+        private BinaryTreeNode right;
+        
+        //Public Properties
+        public char Letter { get => letter; set => letter = value; }
+        public BinaryTreeNode Left { get => left; set => left = value; }
+        public BinaryTreeNode Right { get => right; set => right = value; }
+
+        //Default Constructor
+        public BinaryTreeNode() {
+            Letter = '\0';
+            Left = null;
+            Right = null;
+        }//End BinaryTreeNode Constructor
+    }//End Class LLNode
+}//End Namespace
---
+++ BinaryTree/BinaryTree/BinaryTree/Driver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            //Create a Random Number Generator
+            Random num = new Random();
+
+            for(int i = 0; i < 5; i++)
+            {
+                //Create a new node to add to the tree
+                BinaryTreeNode theNode =  new BinaryTreeNode();
+
+            }//End For Loop
+        }//End Main Method
+    }//End Class Driver
+}//End Namespace
